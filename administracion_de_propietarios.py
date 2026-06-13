b6b7d20651ca86438398dbd043975e137eb0b948
sebadiazq97 <seb.diazq@duocuc.cl>
2026-06-12 20:09:09 -0400

    subida de tarea

---
+++ administracion_de_propietarios.py
@@ -0,0 +1,20 @@
+# funciones requeridas
+# - consultar propietarios
+# - actualizar informacion
+# - agregar nuevos departamentos
+# - eliminar registros
+# - mostrar datos almacenados
+
+def
+
+print ("=======================================")
+print("SISTEMA DE ADMINISTRACION DE PROPIETARIOS")
+print ("=======================================")
+print("")
+print("1.Consultar Propietarios")
+print("2.Actualizar Informacion")
+print("3.Agregar Nuevos Departamentos")
+print("4.Eliminar Registros")
+print("5.Mostrar Datos Almacenados")
+print("6.Salir")
+
